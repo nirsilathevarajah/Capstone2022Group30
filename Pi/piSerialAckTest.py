174e966ba3864662297886ea8745ffd5ea70a76b
Arthur <arthur.faron@gmail.com>
2022-03-15 19:34:09 -0400

    created piSerialHandler to tx cmds to arduino and rx sensor vals

---
+++ Pi/piSerialAckTest.py
@@ -0,0 +1,60 @@
+"""
+serial_usb_simple.py  Demo that communicates over USB using serial I/O
+from a Raspberry Pi to an Arduino.
+
+To show that it work, this writes a '1' to the Arduino which then 
+blinks the builtin LED on and off. The Arduino also sends back an 'A'
+to acknowledge that it got the message. This does a read() to get
+the 'A', demonstrating that reading also works. Two seconds later,
+this writes a '0' to the Arduino which then stops the blinking.
+The Arduino again sends back an 'A' to acknowledge that it got the
+message and this reads the 'A'.
+
+This was tested between a Raspberry Pi 3B (running Raspbian) and
+an Arduino Mega 2560 and also between an NVIDIA Jetson TX1 (running 
+Ubuntu) and the same Arduino.
+"""
+
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyACM0", 56700, timeout=1)
+
+# Reset the Arduino's line. This is key to getting the write to work.
+# Without it, the first few writes don't work.
+# Clear DTR, wait one second, flush input, then set DTR.
+# Without this, the first write fails.
+# This trick was learned from:
+# https://github.com/miguelasd688/4-legged-robot-model
+
+ser.setDTR(False)
+time.sleep(1)
+ser.flushInput()
+ser.setDTR(True)
+time.sleep(2)
+
+while True:
+
+	print('Telling the Arduino to start blinking...')
+	ser.write(b'1')
+
+	# read to get the acknowledgement from the Arduino
+	while True:
+		ack = ser.read()
+		if ack == b'A':
+			break
+	print('Arduino sent back %s' % ack)
+
+	time.sleep(2)
+
+	print('Telling the Arduino to stop blinking...')
+	ser.write(b'0')
+
+	# read to get the acknowledgement from the Arduino
+	while True:
+		ack = ser.read()
+		if ack == b'A':
+			break
+	print('Arduino sent back %s' % ack)
+
+	time.sleep(2)
